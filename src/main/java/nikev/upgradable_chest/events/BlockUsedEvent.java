bc8919d774b5ec9bec717a49c168b51b1d714313
KeTrevis <kevin.trevis@galadrim.fr>
2026-01-18 11:58:49 +0100

    solid bases TODO: upgradable chests

--- src/main/java/nikev/upgradable_chest/events/BlockUsedEvent.java
+++ src/main/java/nikev/upgradable_chest/events/BlockUsedEvent.java
@@ -3,11 +3,24 @@ package nikev.upgradable_chest.events;
 import com.hypixel.hytale.component.Archetype;
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.UseBlockEvent;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.inventory.container.ItemContainer;
+import com.hypixel.hytale.server.core.inventory.container.SimpleItemContainer;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.meta.BlockState;
+import com.hypixel.hytale.server.core.universe.world.meta.state.ItemContainerState;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -27,7 +40,33 @@ public class BlockUsedEvent
         @Nonnull CommandBuffer<EntityStore> commandBuffer,
         @Nonnull UseBlockEvent.Pre event
     ) {
-        System.out.println("hello");
+        InteractionContext context = event.getContext();
+        Ref<EntityStore> ref = context.getEntity();
+        Player player = commandBuffer.getComponent(
+            ref,
+            Player.getComponentType()
+        );
+        if (player == null) {
+            return;
+        }
+        Vector3i target = event.getTargetBlock();
+        World world = store.getExternalData().getWorld();
+        BlockState state = world.getState(target.x, target.y, target.z, true);
+        if (!(state instanceof ItemContainerState itemContainerState)) {
+            return;
+        }
+        short newCapacity = 1;
+        List<ItemStack> remainder = new ObjectArrayList<>();
+        SimpleItemContainer resized = ItemContainer.ensureContainerCapacity(
+            (SimpleItemContainer) itemContainerState.getItemContainer(),
+            newCapacity,
+            SimpleItemContainer::new,
+            remainder
+        );
+        itemContainerState.setItemContainer(resized);
+        player.sendMessage(
+            Message.raw(String.format("chest resized to %d", newCapacity))
+        );
     }
 
     @Nullable
